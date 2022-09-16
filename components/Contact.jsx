956aa918c4a17e3d5ac36402895e981ab03f075b
Stephen Manasseh Rivero Pastor <stephen.m.rivero@gmail.com>
2022-09-16 10:10:51 +0800

    Modify alignment of Component titles & headings
    - Contact.jsx & Projects.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,10 +10,10 @@ const Contact = () => {
   return (
     <div id='contact' className='w-full'>
         <div className='max-w-[1240px] m-auto px-2 pt-16 w-full'>
-            <p className='text-xl tracking-widest uppercase text-[#FF0000]'>
+            <p className='text-xl tracking-widest uppercase text-[#FF0000] pl-5'>
                 Contact
             </p>
-            <h2 className='py-4'>Get In Touch</h2>
+            <h2 className='py-4 pl-5'>Get In Touch</h2>
             <div className='grid lg:grid-cols-5 gap-8'>
                 {/* left */}
                 <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -10,8 +10,8 @@ const Projects = () => {
   return (
     <div id='projects' className='w-full'>
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
-            <p className='text-xl tracking-widest uppercase text-[#FF0000]'>Projects</p>
-            <h2 className='py-4'>What I&apos;ve Built</h2>
+            <p className='text-xl tracking-widest uppercase text-[#FF0000] pl-5'>Projects</p>
+            <h2 className='py-4 pl-5'>What I&apos;ve Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
                 <ProjectItem
                     title='YouTube Clone'
