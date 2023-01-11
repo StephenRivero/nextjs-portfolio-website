d75c5dd041c6a100c0cbc4289e9e9e986e559adf
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-01-12 04:29:12 +0800

    Update About section

--- components/About.jsx
+++ components/About.jsx
@@ -6,14 +6,26 @@ const About = () => {
     <div id='about' className='w-full sm:h-screen p-2 pt-16 pb-36 flex items-center sm:pb-24 sm:pt-24 md:pt-16 xl:py-0'>
         <div className='max-w-[1240px] w-[95%] m-auto lg:grid grid-cols-2 gap-8 relative'>
             <div className='col-span-1'>
-                <p className='px-1 uppercase text-xl tracking-widest text-[#FF0000]'>About</p>
-                <h2 className='py-4 px-1'>Who I Am</h2>
-                <p className='py-2 px-1 text-gray-600'> / I am not your normal developer</p>
+                <p className='px-1 uppercase text-xl tracking-widest text-[#FF0000]'>
+                    About
+                </p>
+                <h2 className='py-4 px-1'>
+                    Who I Am
+                </h2>
+                <p className='py-2 px-1 text-gray-600'>
+                    / I am not your normal developer /
+                </p>
                 <p className='py-2 px-1 text-gray-600 text-justify'>
-                    I have 7 years professional experience as a Licensed Sanitary Engineer in the field of Food Manufacturing, Construction, and Sales Industry. 
-                    Since the passing of my father in September 2021, I&apos;ve never been the same. I&apos;ve struggled a lot. And then, I decided to pursue Front-End Web Development starting in July 2022. 
-                    I&apos;ve learned the fundamentals of, HTML5, CSS3, and JavaScript through the free online course “The Odin Project” where its goal is to help aspiring web developers. 
-                    I&apos;m now spending my time building projects with ReactJs and NextJs and also learning new technologies.
+                    I have over 8 years professional experience as a Licensed Sanitary Engineer in the Engineering Construction industry before becoming a Front-End Web Developer.
+                    Throughout those years my physical & mental health had declined gradually to the point that I&apos;d got overweight & constantly sick due to the set of circumstances as a Project Engineer. 
+
+                    It is during the “Pandemic Season” that I&apos;ve realized that I have to take a drastic change in my career plans. 
+                    After researching & self-assessment, I had decided to pursue Front-End Web Development starting in January 2021 while still working as a Project Engineer. And after a year of learning Front-End Web Development in “
+                    <a href="https://www.theodinproject.com/" className='font-bold xl:hover:cursor-pointer underline'>
+                        The Odin Project
+                    </a>
+                    ”, a free online web development course, I&apos;ve got my first job as a Front-End Web Developer on January 2022.
+                    I&apos;m now spending my free time building projects with ReactJs and NextJs and also learning new technologies.
                 </p>
             </div>
             <div className='w-[100%] sm:w-full lg:mb-4 xl:mb-0 m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
