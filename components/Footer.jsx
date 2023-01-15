bee707488b3b34a0d0f74b507b4a060a134641bf
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-01-15 17:38:42 +0800

    Update Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,20 +10,25 @@ const Footer = () => {
   return (
     <div className='w-full'>
         <div className='flex flex-col sm:flex-row max-w-[1240px] mx-auto justify-center py-2'>
-            <h3 className='flex text-center items-end justify-center'>
-                Built using<p className='text-[#FFFFFF]'>_</p><Image src={reactIcon} alt="An icon of ReactJs" width='32px' height='32px' />ReactJs,
+            <h3 className='flex text-center items-end justify-center gap-1'>
+                Built using
+                <Image src={reactIcon} alt="An icon of ReactJs" width='32px' height='32px' class="px-1"/>
+                ReactJs,
             </h3>
-            <h3 className='flex text-center items-end justify-center'>
-                <p className='text-[#FFFFFF]'>_</p>  
-                <Image src={nextjsIcon} alt="An icon of NextJs" width='32px' height='32px' /><p className='text-[#FFFFFF]'>_</p>NextJs and<p className='text-[#FFFFFF]'>_</p>
+            <h3 className='flex text-center items-end justify-center gap-1 mx-1'>  
+                <Image src={nextjsIcon} alt="An icon of NextJs" width='32px' height='32px' className='px-1'/>
+                NextJs and
             </h3>
-            <h3 className='flex text-center items-end justify-center'>
-                <Image src={tailwindIcon} alt="An icon of Tailwind" width='24px' height='24px' /><p className='text-[#FFFFFF]'>_</p>Tailwind with help from<p className='text-[#FFFFFF]'>_</p><Image src={coffeeCupIcon} alt="An icon of a coffee cup" width='32px' height='32px' />
+            <h3 className='flex text-center items-end justify-center gap-1 mx-1'>
+                <Image src={tailwindIcon} alt="An icon of Tailwind" width='24px' height='24px' className='px-1'/>
+                Tailwind with help from
+                <Image src={coffeeCupIcon} alt="An icon of a coffee cup" width='32px' height='32px' className='px-1'/>
+                .
             </h3>
         </div>
         <div className='flex max-w-[1240px] mx-auto justify-center py-2'>
-            <h3 className='flex text-center items-center justify-center mt-2'>
-                Copyright © Stephen Rivero 2022
+            <h3 className='flex text-center items-center justify-center mt-2 w-[180px] sm:w-full'>
+                Stephen Rivero 2023 © All rights reserved
             </h3>
         </div>
     </div>
