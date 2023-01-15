872656e3c8418eb46a1f6a6be4f1843542dd460a
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-01-15 17:08:24 +0800

    Update Projects section

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -5,14 +5,20 @@ import Image from 'next/image'
 const ProjectItem = ({title, backgroundImg, projectUrl, tech, status}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#FF0000] to-[#ff9292]'>
-        <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' />
-        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[70%]'>
-            <h3 className='text-2xl text-white tracking-wider text-center '>{title}</h3>
-            <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
-            <Link href={projectUrl}>
-                <p className='w-[65%] sm:w-[55%] lg:w-[40%] mx-auto text-center py-2 px-2 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>{status}</p>
-            </Link>
-        </div>
+      <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' />
+      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[80%]'>
+        <h3 className='text-2xl text-white tracking-wider text-center '>
+          {title}
+        </h3>
+        <p className='pb-4 pt-2 text-white text-center mx-auto'>
+          {tech}
+        </p>
+        <Link href={projectUrl}>
+          <p className='w-[65%] sm:w-[55%] lg:w-[50%] mx-auto text-center py-2 px-2 rounded-lg bg-white text-gray-700font-bold text-lg cursor-pointer xl:hover:scale-105'>
+            {status}
+          </p>
+        </Link>
+      </div>
     </div>
   )
 }
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,10 +5,11 @@ import youtubeImg from '../public/assets/projects/youtube-clone-project.jpg';
 import cryptoImg from '../public/assets/projects/crypto.jpg';
 import twitchImg from '../public/assets/projects/twitch.jpg';
 import propertyImg from '../public/assets/projects/property.jpg'
+import metaversusImg from '../public/assets/projects/metaverse-web-app.png'
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full sm:h-screen lg:h-auto'>
+    <div id='projects' className='w-full lg:h-auto'>
         <div className='max-w-[1240px] mx-auto px-2 py-14 xl:pb-0 md:pt-32 lg:pt-16'>
             <p className='text-xl tracking-widest uppercase text-[#FF0000] pt-5 pl-5'>
                 Projects
@@ -17,19 +18,19 @@ const Projects = () => {
                 What I&apos;ve Built
             </h2>
             <div className='grid md:grid-cols-2 gap-8'>
+                <ProjectItem 
+                    title='Metaversus' 
+                    backgroundImg={metaversusImg} 
+                    projectUrl='/property'
+                    tech='Tailwind Css, Framer Motion, React JS, & NextJs-13'
+                    status='More Info' 
+                />
                 <ProjectItem
                     title='YouTube Clone'
                     backgroundImg={youtubeImg}
                     projectUrl='/youtube-clone-project'
-                    tech='React JS, Rapid API, MUI 5'
+                    tech='React JS, Rapid API, & MUI 5'
                     status='More Info'
-                />
-                <ProjectItem 
-                    title='Property Finder' 
-                    backgroundImg={propertyImg} 
-                    projectUrl='/property'
-                    tech='React JS'
-                    status='Under Development' 
                 />
                 <ProjectItem
                     title='Crypto App'
@@ -39,9 +40,9 @@ const Projects = () => {
                     status='Under Development'
                 />
                 <ProjectItem
-                    title='Twitch UI'
-                    backgroundImg={twitchImg}
-                    projectUrl='/twitch'
+                    title='Property Finder'
+                    backgroundImg={propertyImg}
+                    projectUrl='/property'
                     tech='Next JS'
                     status='Under Development'
                 />
