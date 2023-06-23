9b9d761cc4c7c090ff40ea5f3b3bdb2365298ae2
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-23 21:56:11 +0800

    Improve Skills section in 640px width

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -12,8 +12,8 @@ const Skills = () => {
             <h2 className='py-4 pl-2'>
                 What I Can Do
             </h2>
-            <div className='grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8'>
-                <div className='order-1 h-fit p-3 sm:p-6 shadow-lg sm:shadow-xl border border-orange-100 shadow-orange-300 rounded-xl hover:scale-105 ease-in-out duration-200'>
+            <div className='grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-8'>
+                <div className='order-1 h-fit p-3 sm:p-6 shadow-lg border border-orange-100 shadow-orange-300 rounded-xl hover:scale-105 ease-in-out duration-200'>
                     <div className='flex justify-center items-center gap-5'>
                         <div className=''>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="64" height="64" alt="HTML5 logo">
@@ -31,7 +31,7 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>
-                <div className='order-2 h-fit p-3 sm:p-6 shadow-lg sm:shadow-xl border border-blue-100 shadow-blue-600 rounded-xl hover:scale-105 ease-in-out duration-200'>
+                <div className='order-2 h-fit p-3 sm:p-6 shadow-lg border border-blue-100 shadow-blue-600 rounded-xl hover:scale-105 ease-in-out duration-200'>
                     <div className='flex justify-center items-center gap-5'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="62" height="62" alt="CSS3 logo" viewBox="0 0 512 512">
 	                        <path fill="#264de4" d="M71.357 460.819L30.272 0h451.456l-41.129 460.746L255.724 512z"/>
@@ -59,9 +59,9 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>
-                <div className='order-4 h-fit p-[12px] sm:p-6 border border-teal-100 shadow-lg sm:shadow-xl shadow-[#8ac9cf] rounded-xl hover:scale-105 ease-in-out duration-200'>
+                <div className='order-4 h-fit p-[12px] sm:p-6 border border-teal-100 shadow-lg shadow-[#8ac9cf] rounded-xl hover:scale-105 ease-in-out duration-200'>
                     <div className='flex justify-center items-center gap-5'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.116 22.806" width="62" height="62"><g opacity=".8"><path d="M33.71 0L11.207 22.693l.113.113h22.586l11.21-11.304z" fill="#d8dee9"/><path d="M11.405 0l22.578 22.769-.039.037H11.209L0 11.502z" fill="#8ac0cf"/></g></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.116 22.806" width="64" height="64"><g opacity=".8"><path d="M33.71 0L11.207 22.693l.113.113h22.586l11.21-11.304z" fill="#d8dee9"/><path d="M11.405 0l22.578 22.769-.039.037H11.209L0 11.502z" fill="#8ac0cf"/></g></svg>
                         <div className='hidden sm:flex items-center justify-center'>
                             <h3>
                                 AlpineJs
@@ -82,14 +82,17 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>
-                <div className='order-5 h-fit p-3 sm:p-6 border border-cyan-100 shadow-lg sm:shadow-xl shadow-cyan-300 rounded-xl hover:scale-105 ease-in-out duration-200'>
+                <div className='order-5 h-fit p-3 sm:p-6 border border-cyan-100 shadow-lg shadow-cyan-300 rounded-xl hover:scale-105 ease-in-out duration-200'>
                     <div className='flex justify-center items-center gap-5'>
-                        <Image 
-                            src="/assets/skills/react.png" 
-                            width='64px' 
-                            height='64px' 
-                            alt='ReactJs logo' 
-                        />
+                        <svg width={64} height={64} xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
+                          <title>React Logo</title>
+                          <circle cx="0" cy="0" r="2.05" fill="#61dafb"/>
+                          <g stroke="#61dafb" strokeWidth="1" fill="none">
+                            <ellipse rx="11" ry="4.2"/>
+                            <ellipse rx="11" ry="4.2" transform="rotate(60)"/>
+                            <ellipse rx="11" ry="4.2" transform="rotate(120)"/>
+                          </g>
+                        </svg>
                         <div className='hidden sm:flex items-center justify-center'>
                             <h3>
                                 ReactJs
@@ -117,15 +120,13 @@ const Skills = () => {
                 <p className='text-xl tracking-widest uppercase text-[#FF0000] pl-2 mb-4'>
                     Frontend Tools
                 </p>
-                <div className='grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8'>
+                <div className='grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-8'>
                     <div className='h-fit p-3 sm:p-6 border border-teal-100 shadow-lg shadow-teal-300 rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
-                            <Image 
-                                src="/assets/skills/tailwind.png" 
-                                width='64px' 
-                                height='64px' 
-                                alt='/' 
-                            />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="61" height="61" viewBox="0 0 1000 1000" fill="none">
+                                <path d="M489.5 226.499C328 231.632 280 346.999 269 409.499C283.333 386.332 328.5 335.5 395 335.5C472.5 335.5 531.5 422 567.5 449C611.237 481.803 699.123 525.115 814.5 490C906.5 462 949.167 364.332 958.5 317.999C914 378.499 846.5 414.838 763 371.999C705.5 342.499 662.5 221 489.5 226.499Z" fill="#07B6D5"/>
+                                <path d="M261 500.999C99.5 506.132 51.5 621.499 40.5 683.999C54.8333 660.832 100 610 166.5 610C244 610 303 696.5 339 723.5C382.737 756.303 470.623 799.615 586 764.5C678 736.5 720.667 638.832 730 592.499C685.5 652.999 618 689.338 534.5 646.499C477 616.999 434 495.5 261 500.999Z" fill="#07B6D5"/>
+                            </svg>
                             <div className='hidden sm:flex items-center justify-center'>
                                 <h3>
                                     Tailwind CSS
@@ -161,8 +162,8 @@ const Skills = () => {
                 <p className='text-xl tracking-widest uppercase text-[#FF0000] pl-2 mb-4'>
                     Development Tools
                 </p>
-                <div className='grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
-                    <div className='h-fit p-3 sm:p-6 shadow-lg border border-orange-100 shadow-orange-500 rounded-xl hover:scale-105 ease-in-out duration-200'>
+                <div className='grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-4 gap-8'>
+                    <div className='h-fit p-3 sm:p-[23px] shadow-lg border border-orange-100 shadow-orange-500 rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
                             <Image 
                                 src="/assets/skills/git.png" 
@@ -179,12 +180,9 @@ const Skills = () => {
                     </div>
                     <div className='h-fit p-3 sm:p-6 border border-black/10 shadow-lg shadow-black/50 rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
-                            <Image 
-                                src="/assets/skills/github1.png" 
-                                width='64px' 
-                                height='64px' 
-                                alt='Github logo' 
-                            />
+                            <svg width="62" height="62" aria-hidden="true" viewBox="0 0 16 16" version="1.1"  data-view-component="true" className="octicon octicon-mark-github v-align-middle color-fg-default">
+                                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                            </svg>
                             <div className='hidden sm:flex items-center justify-center'>
                                 <h3>
                                     GitHub
@@ -194,7 +192,7 @@ const Skills = () => {
                     </div>
                     <div className='h-fit p-3 sm:p-6 border border-blue-100 shadow-lg shadow-blue-500 rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" alt="Bitbucket logo" viewBox="0 0 62.42 62.42">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="62" height="62" alt="Bitbucket logo" viewBox="0 0 62.42 62.42">
                                 <defs>
                                     <linearGradient id="New_Gradient_Swatch_1" x1="64.01" y1="30.27" x2="32.99" y2="54.48" gradientUnits="userSpaceOnUse">
                                         <stop offset="0.18" stopColor="#0052cc"/>
@@ -216,7 +214,7 @@ const Skills = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='h-fit p-3 sm:p-6 border border-black/10 shadow-xl shadow-black/50 rounded-xl hover:scale-105 ease-in-out duration-200'>
+                    <div className='h-fit p-3 sm:p-6 border border-black/10 shadow-lg shadow-black/50 rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
                             <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="64" height="64" alt="Vercel logo" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
                                 <metadata>
@@ -235,7 +233,7 @@ const Skills = () => {
                     </div>
                     <div className='h-fit p-3 sm:p-6 border border-green-100 shadow-lg shadow-[#68A063] rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
-                            <svg className='h-fit' xmlns="http://www.w3.org/2000/svg" width="256px" height="289px" viewBox="0 0 256 289" version="1.1" preserveAspectRatio="xMidYMid">
+                            <svg className='h-[64px]' xmlns="http://www.w3.org/2000/svg" width="256px" height="289px" viewBox="0 0 256 289" version="1.1" preserveAspectRatio="xMidYMid">
                                 <g>
                                     <path d="M127.999999,288.463771 C124.024844,288.463771 120.314699,287.403728 116.869564,285.548656 L81.6231884,264.612838 C76.32298,261.697724 78.9730854,260.637682 80.5631458,260.107661 C87.7184259,257.72257 89.0434775,257.192547 96.4637688,252.952381 C97.2587979,252.422361 98.3188405,252.687372 99.1138718,253.217392 L126.144927,269.383024 C127.20497,269.913045 128.530021,269.913045 129.325053,269.383024 L235.064182,208.165634 C236.124225,207.635611 236.654245,206.575571 236.654245,205.250519 L236.654245,83.0807467 C236.654245,81.7556929 236.124225,80.6956526 235.064182,80.1656324 L129.325053,19.2132506 C128.26501,18.6832305 126.939959,18.6832305 126.144927,19.2132506 L20.4057954,80.1656324 C19.3457551,80.6956526 18.8157349,82.0207041 18.8157349,83.0807467 L18.8157349,205.250519 C18.8157349,206.31056 19.3457551,207.635611 20.4057954,208.165634 L49.2919247,224.861286 C64.9275364,232.811595 74.7329196,223.536234 74.7329196,214.260871 L74.7329196,93.681159 C74.7329196,92.0910985 76.0579711,90.5010358 77.9130428,90.5010358 L91.4285716,90.5010358 C93.0186343,90.5010358 94.6086948,91.8260873 94.6086948,93.681159 L94.6086948,214.260871 C94.6086948,235.196689 83.2132512,247.387164 63.3374737,247.387164 C57.2422362,247.387164 52.4720502,247.387164 38.9565214,240.761906 L11.1304347,224.861286 C4.24016581,220.886129 5.68434189e-14,213.46584 5.68434189e-14,205.515528 L5.68434189e-14,83.3457557 C5.68434189e-14,75.3954465 4.24016581,67.9751552 11.1304347,64.0000006 L116.869564,2.78260752 C123.494824,-0.927535841 132.505176,-0.927535841 139.130436,2.78260752 L244.869565,64.0000006 C251.759834,67.9751552 256,75.3954465 256,83.3457557 L256,205.515528 C256,213.46584 251.759834,220.886129 244.869565,224.861286 L139.130436,286.078676 C135.685299,287.668739 131.710145,288.463771 127.999999,288.463771 L127.999999,288.463771 Z M160.596274,204.455488 C114.219461,204.455488 104.679089,183.254659 104.679089,165.233955 C104.679089,163.643893 106.004141,162.053832 107.859212,162.053832 L121.639752,162.053832 C123.229813,162.053832 124.554864,163.113872 124.554864,164.703935 C126.674947,178.749484 132.770187,185.639753 160.861283,185.639753 C183.122154,185.639753 192.662526,180.604556 192.662526,168.67909 C192.662526,161.788821 190.012423,156.753624 155.296065,153.308489 C126.409938,150.393375 108.389235,144.033126 108.389235,120.977226 C108.389235,99.5113875 126.409938,86.7908901 156.621119,86.7908901 C190.542443,86.7908901 207.238095,98.4513472 209.358178,123.89234 C209.358178,124.687371 209.093167,125.482403 208.563147,126.277434 C208.033127,126.807454 207.238095,127.337474 206.443064,127.337474 L192.662526,127.337474 C191.337475,127.337474 190.012423,126.277434 189.747412,124.952382 C186.567289,110.376813 178.351966,105.606625 156.621119,105.606625 C132.240165,105.606625 129.325053,114.086957 129.325053,120.447205 C129.325053,128.132506 132.770187,130.5176 165.631471,134.757766 C198.227744,138.997931 213.598344,145.093169 213.598344,167.884058 C213.333333,191.20497 194.252589,204.455488 160.596274,204.455488 L160.596274,204.455488 Z" fill="#539E43"/>
                                 </g>
@@ -249,7 +247,7 @@ const Skills = () => {
                     </div>
                     <div className='h-fit p-3 sm:p-6 border border-red-100 shadow-lg shadow-red-400 rounded-xl hover:scale-105 ease-in-out duration-200'>
                         <div className='flex justify-center items-center gap-5'>
-                            <svg className='h-fit py-[20px]' xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="540px" height="210px" viewBox="0 0 18 7">
+                            <svg className='h-[64px] py-[20px]' xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="540px" height="210px" viewBox="0 0 18 7">
                                 <path fill="#CB3837" d="M0,0h18v6H9v1H5V6H0V0z M1,5h2V2h1v3h1V1H1V5z M6,1v5h2V5h2V1H6z M8,2h1v2H8V2z M11,1v4h2V2h1v3h1V2h1v3h1V1H11z"/>
                                 <polygon fill="#FFFFFF" points="1,5 3,5 3,2 4,2 4,5 5,5 5,1 1,1 "/>
                                 <path fill="#FFFFFF" d="M6,1v5h2V5h2V1H6z M9,4H8V2h1V4z"/>
@@ -263,7 +261,7 @@ const Skills = () => {
                         </div>
                     </div>
                     <div className='h-fit p-3 sm:p-6 border border-blue-100 shadow-lg shadow-[#0078d7] rounded-xl hover:scale-105 ease-in-out duration-200'>
-                        <div className='flex justify-center items-center gap-5'>
+                        <div className='flex justify-center items-center gap-5 py-[2.5px]'>
                             <Image 
                                 src="/assets/skills/Visual_Studio_Code_1.35_icon.svg.png" 
                                 width='64px' 
