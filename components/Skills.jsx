1cccb1b51db01eb0230290f9a9e43fb0680863e5
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-24 12:31:36 +0800

    Improve arrangement/alignment
    Improved arrangement/aligment of Skills component starting
    width 1280px and beyond

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
   return (
     <div id='skills' className='w-full 2xl:h-screen pb-16 pt-16 lg:pt-14 2xl:pt-16  p-2 sm:p-5 flex justify-center xl:items-center'>
         <div className='max-w-[1240px] lg:w-[95%] mx-auto flex flex-col justify-center h-full'>
-            <p className='text-xl tracking-widest uppercase text-[#FF0000] pl-2 2xl:pt-5 xl:mb-[15px] xl:ml-[140px] xl:mt-[16px]'>
+            <p className='text-xl tracking-widest uppercase text-[#FF0000] pl-2 2xl:pt-5 xl:mb-[16px] xl:ml-[140px] xl:mt-[16px]'>
                 Skills
             </p>
             <h2 className='xl:hidden py-4 pl-2'>
